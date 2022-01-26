ae897404acd1a5b60ecea29f8c03661aaf6d7bcd
adamhinton <adam.d.hinton@gmail.com>
2022-01-26 13:02:08 -0500

    wip

--- index.test.js
+++ index.test.js
@@ -29,9 +29,23 @@ describe("[Exercise 2] trimPropertiesMutation", () => {
   });
 });
 
-// describe("[Exercise 3] findLargestInteger", () => {
-//   // test('[5] returns the largest number in an array of objects { integer: 2 }', () => {})
-// });
+describe("[Exercise 3] findLargestInteger", () => {
+  test("[5] returns the largest number in an array of objects { integer: 2 }", () => {
+    const input1 = [{ integer: 1 }, { integer: 3 }, { integer: 2 }];
+    const input2 = [{ integer: 15 }, { integer: 543533 }, { integer: 26 }];
+    const input3 = [
+      { integer: 5232223566775441 },
+      { integer: 36 },
+      { integer: 2 },
+    ];
+    const actual1 = utils.findLargestInteger(input1);
+    expect(actual1).toBe(3);
+    const actual2 = utils.findLargestInteger(input2);
+    expect(actual2).toBe(543533);
+    const actual3 = utils.findLargestInteger(input3);
+    expect(actual3).toBe(5232223566775441);
+  });
+});
 
 // describe("[Exercise 4] Counter", () => {
 //   let counter;
